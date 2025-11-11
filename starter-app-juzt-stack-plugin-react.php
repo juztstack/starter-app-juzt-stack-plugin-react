f8e6c7aa1de799c8bf785cd0166b16d3c9cf90cc
JESÚS UZCATEGUI <74186693+jesusuzcategui@users.noreply.github.com>
2025-11-11 11:53:59 -0500

    Add main file for the plugin

---
+++ starter-app-juzt-stack-plugin-react.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Plugin Name: Starter App Juzt Stack Plugin React
+ * Plugin URI: https://github.com/juztstack/starter-app-juzt-stack-plugin-react
+ * Description: Example for Application react based on plugin wordpress
+ * Version: 1.7.0
+ * Author: Jesus Uzcategui
+ * Author URI: https://github.com/juztstack
+ * License: MIT License
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: starter-app-juzt-stack-plugin-react
+ * Domain Path: /languages
+ * Requires at least: 5.0
+ * Tested up to: 6.4
+ * Requires PHP: 7.4
+ * Network: false
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
